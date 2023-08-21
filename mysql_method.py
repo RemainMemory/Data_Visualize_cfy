0ba48447029d6b41977f01a2722ec24b5ac713b3
Lionce <sakura.xue8@gmail.com>
2023-08-21 18:04:13 +0800

    adjust the functionality

--- mysql_method.py
+++ mysql_method.py
@@ -5,7 +5,7 @@ DB_CONFIG = {
     "host": "localhost",
     "port": 3306,
     "user": "root",
-    "password": "KINGISME24??",
+    "password": "123456",
     "db": "SD_data",
     "charset": "utf8mb3"
 }
